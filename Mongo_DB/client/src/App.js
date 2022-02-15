c8b55296ffcb2269c5dce46d34db690598103738
baek1008 <junu0810@hanmail.net>
2022-02-15 22:17:07 +0900

    connect client

--- Mongo_DB/client/src/App.js
+++ Mongo_DB/client/src/App.js
@@ -1,11 +1,19 @@
+import axios from 'axios'
 
 
 function App() {
+  const timesubmit = () => {
+    console.log('날짜전송')
+    axios.post('http://localhost:4000/post')
+  }
+
   return (
-    <div>
-      <div>Mongo_DB</div>
-    </div>
-  )
+  <div>
+    <button onClick={timesubmit}>
+      날짜전송하기
+    </button>
+  </div>
+    )
 }
 
 export default App;
--- Mongo_DB/server/server.js
+++ Mongo_DB/server/server.js
@@ -6,30 +6,6 @@ const PORT = 4000
 const morgan = require('morgan')
 const Mongo = require('mongodb').MongoClient
 
-let db;
-Mongo.connect(`mongodb+srv://Baek:${process.env.DB_PASSWORD}@cluster0.jolds.mongodb.net/myFirstDatabase?retryWrites=true&w=majority` , (err, client)=>{
-
-if(err){
-  console.log('서버연결에 실패했습니다.')
-  console.log(err)
-}
-else{
-   
-  //learn_database에 연결할 예정 
-  db = client.db('learn_database')
-
-  db.collection('post').insertOne({type:'testData' , id:1} ,(err, result) => {
-    console.log('저장이 완료되었습니다.')
-  })
-
-  //connect완료시 작성
-  app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-  })
-}
-})
-
-
 // app.use(nodemon())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
@@ -37,15 +13,51 @@ app.use(morgan('dev'))
 app.use(
   cors({
     credentials: true,
- origin: process.env.ORIGIN,
+    origin: process.env.ORIGIN,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   })
 )
 
-app.use('/', (req, res)=> {
-  res.status(200).send('서버 개발 완료')
+let db;
+Mongo.connect(`mongodb+srv://Baek:${process.env.DB_PASSWORD}@cluster0.jolds.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, (err, client) => {
+
+  if (err) {
+    console.log('서버연결에 실패했습니다.')
+    console.log(err)
+  }
+  else {
+    db = client.db('learn_database')
+  }
+  // else{
+  //   //learn_database에 연결할 예정 
+  //   db = client.db('learn_database')
+
+  //   db.collection('post').insertOne({type:'testData' , id:1} ,(err, result) => {
+  //     console.log('저장이 완료되었습니다.')
+  //   })
+
+  //   //connect완료시 작성
+  //   
 })
 
+// app.get('/', (req, res) => {
+//   res.status(200).send('서버 개발 완료')
+// })
 
+app.get('/post', (req, res) => {
+ const result = db.collection('post').insertOne({ type: Date(), id: 1 }, (err, result) => {
+    if(err){
+      return res.status(400).send(err)
+    }
+    else{
+      return res.status(200).send(Date())
+    }
+  })
+  return result
+})
+
+app.listen(PORT, () => {
+      console.log(`listening on port ${PORT}`)
+})
 
 module.exports = app
